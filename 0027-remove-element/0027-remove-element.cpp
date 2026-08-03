d62bc3419d2bf0bfba76db25c7ca5b56f56b973e
Natiq Javid <natiqjavid1514@gmail.com>
2026-08-03 19:31:24 +0530

    Time: 0 ms (100%), Space: 11.9 MB (12.99%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        auto it=nums.begin();
+        while(it!=nums.end()){
+            if(*it==val){
+                nums.erase(it);
+            }
+            else{
+                it++;
+            }
+        }
+      int size=nums.size();
+      return size;
+
+    }
+};
